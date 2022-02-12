903dd9656e18aa8d8ff025ccbd141d9969347c97
Doug Niccum <doug.niccum@schierproducts.com>
2022-02-12 13:20:38 -0600

    Additional dictionary enry.

--- resources/lang/en/nova.php
+++ resources/lang/en/nova.php
@@ -1,12 +1,15 @@
 <?php
 
 return [
+    'available_actions' => 'Available Actions',
     'name' => 'Name',
     'name_help' => 'Provide a helpful name/label for the webhook you are about to create; like the site and/or application that will consume it.',
     'name_placeholder' => 'A helpful title',
     'last_modified_by' => 'Last Modified By',
+    'no_actions_available' => 'You currently do not have any actions available. Please add the necessary traits to your application\'s models to select actions for this webhook.',
     'secret' => 'Secret',
     'secret_help' => 'If necessary provide a secret that will be used to validate this hook. If not, one will be generated for you during creation.',
+    'settings' => 'Settings',
     'url' => 'Url',
     'url_help' => 'The url or "recipient" of the webhook when it is dispatched by this application.',
 ];
