6aaa517736411a64ee637347582c0c47019fdf4a
Doug Niccum <doug.niccum@schierproducts.com>
2022-02-12 13:23:43 -0600

    Adds the ability to customize the name of the model that is shown in the list.

--- src/Contracts/WebhookModel.php
+++ src/Contracts/WebhookModel.php
@@ -12,17 +12,24 @@ class WebhookModel
     public $name;
 
     /**
+     * @var string
+     */
+    public $label;
+
+    /**
      * @var string[]
      */
     public $actions = [];
 
     /**
      * @param string $name
+     * @param string $label
      * @param string[] $actions
      */
     public function __construct(string $name, $actions = [])
     {
         $this->name = $name;
+        $this->label = method_exists($name, 'webhookLabel') ? $name::webhookLabel() : $name;;
         $this->actions = $actions;
     }
 
@@ -35,5 +42,23 @@ class WebhookModel
         if (ModelEvents::hasValue($action) && !in_array($action, $this->actions)) {
             array_push($this->actions, $action);
         }
+    }
+
+    /**
+     * @param string $action
+     * @return string
+     */
+    public function actionName(string $action) : string
+    {
+        return $this->name.':'.$action;
+    }
+
+    /**
+     * @param string $action
+     * @return string
+     */
+    public function label(string $action) : string
+    {
+        return $this->label.':'.$action;
     }
 }
