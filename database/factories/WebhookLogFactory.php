bd7a800d34341f033dcd945b0831bf30db1c038b
Doug Niccum <doug.niccum@schierproducts.com>
2022-03-04 08:13:08 -0600

    Adds migration stub and factory for new WebhookLog model.

---
+++ database/factories/WebhookLogFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dniccum\NovaWebhooks\Database\Factories;
+
+use Dniccum\NovaWebhooks\Models\WebhookLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WebhookLogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = WebhookLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'successful' => true,
+        ];
+    }
+
+    /**
+     * @return WebhookLogFactory|m.\Dniccum\NovaWebhooks\Database\Factories\WebhookLogFactory.state
+     */
+    public function notFound()
+    {
+        return $this->state(function(array $attributes) {
+            return [
+                'successful' => false,
+                'error_code' => '404',
+                'error_message' => 'Page not found.'
+            ];
+        });
+    }
+
+    /**
+     * @return WebhookLogFactory|m.\Dniccum\NovaWebhooks\Database\Factories\WebhookLogFactory.state
+     */
+    public function failed()
+    {
+        return $this->state(function(array $attributes) {
+            return [
+                'successful' => false,
+                'error_code' => '422',
+                'error_message' => $this->faker->text,
+            ];
+        });
+    }
+}
