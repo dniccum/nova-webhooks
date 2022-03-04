54a27f80fcf6c043efc4229b8a5185fe2c2b817c
Doug Niccum <doug.niccum@schierproducts.com>
2022-03-04 08:12:35 -0600

    Adds logging configuration setting.

--- config/nova-webhooks.php
+++ config/nova-webhooks.php
@@ -8,6 +8,14 @@ return [
     'enabled' => env('NOVA_WEBHOOKS_ENABLED', true),
 
     /**
+     * If logging should be enabled for each successful/failed request
+     */
+    'logging' => [
+
+        'enabled' => env('NOVA_WEBHOOKS_LOGGING_ENABLED', true),
+    ],
+
+    /**
      * The Laravel Nova resource that manages your authenticated users.
      */
     'users' => [
